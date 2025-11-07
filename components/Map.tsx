9015d65a26058a4387e4f901d422d79146d913c7
Héctor Sisternas <hsisternas@redsauce.net>
2025-11-07 19:49:44 +0100

    Cambios en el componente map

--- components/Map.tsx
+++ components/Map.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef, useState } from 'react';
 import type { Resource, Geolocation } from '../types';
 import { useTranslation } from '../contexts/LanguageContext';
 
-const GOOGLE_MAPS_API_KEY = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+const GOOGLE_MAPS_API_KEY = process.env.API_KEY;
 
 let scriptLoadingPromise: Promise<void> | null = null;
 
