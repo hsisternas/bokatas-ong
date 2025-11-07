12526b9c388260be63f472a7849151c3e18707eb
Héctor Sisternas <hsisternas@redsauce.net>
2025-11-07 20:00:41 +0100

    Changes

--- components/icons/CategoryIcons.tsx
+++ components/icons/CategoryIcons.tsx
@@ -11,18 +11,17 @@ const Icon: React.FC<{ iconName: string; className?: string }> = ({ iconName, cl
 );
 
 // Se exportan los componentes de iconos con el nombre del icono de Material Symbols correspondiente
-// FIX: Changed type from React.SVGProps<SVGSVGElement> to { className?: string } to match the rendered <span> element.
-export const InfoIcon: React.FC<{ className?: string }> = (props) => <Icon {...props} iconName="info" />;
-export const BedIcon: React.FC<{ className?: string }> = (props) => <Icon {...props} iconName="bed" />;
-export const FoodIcon: React.FC<{ className?: string }> = (props) => <Icon {...props} iconName="restaurant" />;
-export const ShowerIcon: React.FC<{ className?: string }> = (props) => <Icon {...props} iconName="shower" />;
-export const HealthIcon: React.FC<{ className?: string }> = (props) => <Icon {...props} iconName="health_and_safety" />;
-export const ClothesIcon: React.FC<{ className?: string }> = (props) => <Icon {...props} iconName="checkroom" />;
-export const WorkIcon: React.FC<{ className?: string }> = (props) => <Icon {...props} iconName="work" />;
-export const LegalIcon: React.FC<{ className?: string }> = (props) => <Icon {...props} iconName="gavel" />;
-export const BookIcon: React.FC<{ className?: string }> = (props) => <Icon {...props} iconName="menu_book" />;
-export const WomanIcon: React.FC<{ className?: string }> = (props) => <Icon {...props} iconName="woman" />;
-export const CompassIcon: React.FC<{ className?: string }> = (props) => <Icon {...props} iconName="explore" />;
-export const StreetIcon: React.FC<{ className?: string }> = (props) => <Icon {...props} iconName="emoji_people" />;
-export const MapIcon: React.FC<{ className?: string }> = (props) => <Icon {...props} iconName="map" />;
-export const OtherIcon: React.FC<{ className?: string }> = (props) => <Icon {...props} iconName="more_horiz" />;}
+export const InfoIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => <Icon {...props} iconName="info" />;
+export const BedIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => <Icon {...props} iconName="bed" />;
+export const FoodIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => <Icon {...props} iconName="restaurant" />;
+export const ShowerIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => <Icon {...props} iconName="shower" />;
+export const HealthIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => <Icon {...props} iconName="health_and_safety" />;
+export const ClothesIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => <Icon {...props} iconName="checkroom" />;
+export const WorkIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => <Icon {...props} iconName="work" />;
+export const LegalIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => <Icon {...props} iconName="gavel" />;
+export const BookIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => <Icon {...props} iconName="menu_book" />;
+export const WomanIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => <Icon {...props} iconName="woman" />;
+export const CompassIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => <Icon {...props} iconName="explore" />;
+export const StreetIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => <Icon {...props} iconName="emoji_people" />;
+export const MapIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => <Icon {...props} iconName="map" />;
+export const OtherIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => <Icon {...props} iconName="more_horiz" />;
