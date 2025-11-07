7c6f7ac3ef47ce6aa3ac3d5f075d4cb5974d62e7
Héctor Sisternas <hsisternas@redsauce.net>
2025-11-07 19:53:25 +0100

    Cambio la llamada a la api

--- components/Map.tsx
+++ components/Map.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef, useState } from 'react';
 import type { Resource, Geolocation } from '../types';
 import { useTranslation } from '../contexts/LanguageContext';
 
-const GOOGLE_MAPS_API_KEY = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
+const GOOGLE_MAPS_API_KEY = process.env.GOOGLE_MAPS_API_KEY;
 
 let scriptLoadingPromise: Promise<void> | null = null;
 
